817476630969e5b1090b2d0a293d5f4153986257
Santosh-snowpro <121649148+santosh-snowpro@users.noreply.github.com>
2022-12-29 20:35:37 -0600

    Update streamlit_app.py
    added pandas 
    step to read data from file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy Diner')
 
@@ -9,3 +10,6 @@ streamlit.text(" \N{flexed biceps} 1--> Idli")
 streamlit.text(" \N{brain} 2--> Vada")
 streamlit.text(" 2--> Upma")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
