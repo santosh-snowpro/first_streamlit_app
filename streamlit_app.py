c05aae2a9f1ec285167acc98434901c7cb05f0fa
Santosh-snowpro <121649148+santosh-snowpro@users.noreply.github.com>
2022-12-29 20:19:00 -0600

    Update streamlit_app.py
    added streamlit header for heading
    streamlit text for individual items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy Diner')
 
-streamlit.title(" Break Fast")
+streamlit.header(" Break Fast")
+
+streamlit.text(" 1--> Idli")
+
+streamlit.text(" 2--> Vada")
