2cbb5557ab6784919e363385f23f9aaec344d51c
Santosh-snowpro <121649148+santosh-snowpro@users.noreply.github.com>
2022-12-29 20:28:04 -0600

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My parents new healthy Diner')
 
 streamlit.header(" Break Fast")
 
-streamlit.text(" 1--> Idli")
+streamlit.text(" \N{flexed biceps} 1--> Idli")
 
 streamlit.text(" 2--> Vada")
 streamlit.text(" 2--> Upma")
